e40578c5a7d29f658e4ee725a7794955616c8674
sooheyon <sooheyonjo@gmail.com>
2023-11-07 12:52:52 +0900

    styled, style reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const container = document.getElementById("root") as HTMLElement;
 
+const GlobalStyled = createGlobalStyle`${reset}`;
 
 ReactDOM.createRoot(container).render(
   <>
+    <GlobalStyled />
     <App />
-  </>,
+  </>
 );
